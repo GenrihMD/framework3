3af3a53189c0e5000703465e4a16c231c8ca8105
Лямин <genrih.md@gmail.com>
2021-03-14 19:37:41 +0300

    Add list

---
+++ src/components/List.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="list">
+    <ul>
+      <li v-for="sub in subs" :key="sub.time">
+        {{ sub.time }} : {{ sub.text }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+const subList = [
+  {
+    time: new Date(),
+    text: "luhgkgvb"
+  }
+];
+
+export default defineComponent({
+  name: "List",
+
+  setup() {
+    const subs = ref(subList);
+
+    setInterval(() => {
+      subs.value.push({
+        time: new Date(),
+        text: "luhgkgvb"
+      });
+    }, 1000);
+
+    return {
+      subs
+    };
+  }
+});
+</script>
