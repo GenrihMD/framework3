8638379a00977f8ff2c76c9c2a33b1ad1e53fdd1
laamin <genrih.md@gmail.com>
2021-05-23 14:19:44 +0300

    Add store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,16 @@
-import { createStore } from "vuex";
+import { createStore } from "vuex-smart-module";
+import { foo } from "./foo.worker";
 
-export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+console.log(foo);
+
+// The 1st argument is root module.
+// Vuex store options should be passed to the 2nd argument.
+export const store = createStore(
+  // Root module
+  foo,
+
+  // Vuex store options
+  {
+    strict: process.env.NODE_ENV !== "production"
+  }
+);
